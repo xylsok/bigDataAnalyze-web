12eb79d22bf9066c9a3f5d8210f4ce5f974160d4
张振峰 <xyls_ok@126.com>
2018-11-12 17:40:38 +0800

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ciPinFenXiWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ciPinFenXiWebApp
+ */
+angular.module('ciPinFenXiWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
